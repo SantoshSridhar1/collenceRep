6ad22c323cee4fa6c8c713d668d90450aeb0ffe4
Santosh Sridhar <santosh.sridhar1@wipro.com>
2017-05-06 17:28:13 +0530

    first commit

---
+++ src/test/java/com/wipro/Entity/MainClassTest.java
@@ -0,0 +1,14 @@
+package com.wipro.Entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainClassTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
